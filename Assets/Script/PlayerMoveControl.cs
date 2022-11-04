ea22664a559cb51019ef255a1b8a6a7a2db63c46
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-04 10:38:50 +0700

    +add move, move control player

--- Assets/Script/PlayerMoveControl.cs
+++ Assets/Script/PlayerMoveControl.cs
@@ -7,6 +7,9 @@ public class PlayerMoveControl : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float editForwardForce = 500f;
+    public float editSizeWayForce = 200f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,14 @@ public class PlayerMoveControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        rb.AddForce(0, 0, editForwardForce * Time.deltaTime);
         
+        if(Input.GetKey("a")){
+            rb.AddForce(-editForwardForce * Time.deltaTime, 0, 0);
+        }
+
+         if(Input.GetKey("d")){
+            rb.AddForce(editForwardForce * Time.deltaTime, 0, 0);
+        }
     }
 }
