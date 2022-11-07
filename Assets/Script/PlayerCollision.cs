1cd5e3a2fdc1347f86a0baf839c3b01bef5112f0
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-07 08:33:34 +0700

    +add collision gameplay

---
+++ Assets/Script/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMoveControl moveControl;
+    void OnCollisionEnter(Collision collisionInfo){
+        if(collisionInfo.collider.tag == "Wall"){
+            moveControl.enabled = false;
+        }
+    }
+}
